8b15cdc40578666be2c9c2fbf8466faff071c4d0
sourav-maji <s722143@gmail.com>
2020-09-04 16:55:16 +0530

    auth-guard

--- BackEnd/routes/auth.js
+++ BackEnd/routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { body } = require('express-validator/check');
+const {
+  body
+} = require('express-validator/check');
 const isAuth = require('../middleware/is-auth');
 
 const User = require('../models/user');
@@ -8,32 +10,39 @@ const authController = require('../controllers/auth');
 const router = express.Router();
 
 router.post(
-    '/signup',
-    [
-      body('email')
-        .isEmail()
-        .withMessage('Please enter a valid email.')
-        .custom((value, { req }) => {
-          return User.findOne({ email: value }).then(userDoc => {
-            if (userDoc) {
-              return Promise.reject('E-Mail address already exists!');
-            }
-          });
-        })
-        .normalizeEmail(),
-      body('password')
-        .trim()
-        .isLength({ min: 5 }),
-      body('name')
-        .trim()
-        .not()
-        .isEmpty()
-    ],
-    authController.signup
-  );
-  
-  router.post('/login', authController.login);
-  // router.get('/chat', isAuth, authController.getUserStatus);
+  '/signup',
+  [
+    body('email')
+    .isEmail()
+    .withMessage('Please enter a valid email.')
+    .custom((value, {
+      req
+    }) => {
+      return User.findOne({
+        email: value
+      }).then(userDoc => {
+        if (userDoc) {
+          alert('Email already exist')
+          return Promise.reject('E-Mail address already exists!');
+        }
+      });
+    })
+    .normalizeEmail(),
+    body('password')
+    .trim()
+    .isLength({
+      min: 5
+    }),
+    body('name')
+    .trim()
+    .not()
+    .isEmpty()
+  ],
+  authController.signup
+);
+
+router.post('/login', authController.login);
+// router.get('/chat', isAuth, authController.getUserStatus);
 
 
-  module.exports = router;
+module.exports = router;
